40f33d1440bf1f59f340b6ce648f821111501b14
Birkin James Diana <birkin.diana@gmail.com>
2023-07-17 09:37:14 -0400

    implements version response.

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from x_app import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    ## main ---------------------------------------------------------
+    path( 'info/', views.info, name='info_url' ),
+    ## other --------------------------------------------------------
+    path( '', views.root, name='root_url' ),
+    path( 'admin/', admin.site.urls ),
+    path( 'error_check/', views.error_check, name='error_check_url' ),
+    path( 'version/', views.version, name='version_url' ),
 ]
