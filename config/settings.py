cb3fa0386292322b14cc4ecd6a656688a59cb297
Birkin James Diana <birkin.diana@gmail.com>
2024-08-26 13:22:29 -0400

    updates app name.

--- config/settings.py
+++ config/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ '%s/x_app' % BASE_DIR ],
+        'DIRS': [ '%s/foo_app' % BASE_DIR ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -173,7 +173,7 @@ LOGGING = {
             'level': 'ERROR',
             'propagate': False,
         },
-        'x_app': {
+        'foo_app': {
             'handlers': ['logfile'],
             'level': os.environ['XPRJCT_42__LOG_LEVEL'],
             'propagate': False
